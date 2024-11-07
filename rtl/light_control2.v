aad5d5aac8f511925b34ce4c8e6c43e7edabb48b
onur nuri turan <onurnuri.turan@std.yeditepe.edu.tr>
2024-11-07 12:07:10 +0300

    ventilation system

--- rtl/light_control2.v
+++ rtl/light_control2.v
@@ -4,11 +4,13 @@
 
 module light_control2(out1,in1,in2);
 
-input in1, in2; // girişler
+input in1, in2; // in1 is for light(day or night) input.in2 is for person.If there is a person in the room in2 =1
 output out1;
  wire not1;
  
  assign not1=~in1;
  assign out1 = not1 & in2;
-
+//if there is no light and a person in the room than open the light
+//if there is no light and none person in the room than don't open the light
+//if there is light don't open the light
 endmodule
